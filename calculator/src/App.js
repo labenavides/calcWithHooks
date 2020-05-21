f72c6c3f4d5e82424863bd096115a574d579a445
lesly <leslybenavides@gmail.com>
2020-05-21 19:20:55 -0400

    Operations component renamed.

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
-import OperationProvider from "./components/OperationsProvider";
+import Calculator from "./components/Calculator";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <OperationProvider></OperationProvider>
+      <Calculator></Calculator>
     </div>
   );
 }
